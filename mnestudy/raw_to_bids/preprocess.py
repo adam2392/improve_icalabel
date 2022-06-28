6c9d4f0d5b62a3aa5442c8e80f24f02f4d9583d6
mscheltienne <mathieu.scheltienne@fcbg.ch>
2022-06-28 14:05:18 +0200

    fix typo and remove trailing spaces

--- mnestudy/raw_to_bids/preprocess.py
+++ mnestudy/raw_to_bids/preprocess.py
@@ -8,7 +8,7 @@ from mne.preprocessing import ICA, compute_bridged_electrodes
 def preprocess_ANT_dataset(raw):
     """Automatic preprocessing pipeline for ANT dataset."""
     # Bandpass standard filter
-    # 100 Hz edge shhould retain muscle activity.
+    # 100 Hz edge should retain muscle activity.
     bandpass = (1., 100.)  # Hz
     raw.filter(
         l_freq=bandpass[0],
@@ -22,28 +22,28 @@ def preprocess_ANT_dataset(raw):
     )
     # Set montage
     raw.set_montage("standard_1020")
-    
+
     # Detect bad channels, on a copy
     nc = pyprep.find_noisy_channels.NoisyChannels(raw.copy())
     nc.find_all_bads()
     raw.info["bads"] = nc.get_bads()
-    
+
     # Look for bridged electrodes
     # requires MNE >= 1.1.0
     bridged_idx, _ = compute_bridged_electrodes(raw)
     ch_names = [raw.ch_names[k] for k in itertools.chain(*bridged_idx)]
     raw.info["bads"] = list(set(raw.info["bads"] + ch_names))
-    
+
     # CAR reference
     # The reference channel CPz is not added as it would just introduce an
     # additional dependency and is not required for the ICA.
     # The CAR reference excludes the bad channels.
     raw.set_eeg_reference("average", ch_type="eeg", projection=False)
-    
+
     # Fit ICA decomposition on good channels
     # Fit on n_good_channels-1 because of CAR reference.
     picks = mne.pick_types(raw.info, eeg=True, exclude="bads")
     ica = ICA(n_components=picks.size - 1, method="picard")
     ica.fit(raw, picks=picks)
-    
+
     return raw, ica
