74b4e49e31a1df2627079f0474004db826164ba5
Anand Saini <asaini@fcbg.ch>
2022-09-02 17:20:17 +0200

    Update mul_percep.py
    Labels based on: if logit>0.7 then 1 else 0.

--- mnestudy/models/mul_percep.py
+++ mnestudy/models/mul_percep.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 import torch
 from matplotlib import pyplot as plt
 from torch.utils.data import Dataset, DataLoader
@@ -10,6 +11,10 @@ from sklearn.preprocessing import LabelEncoder
 from torch.nn.functional import binary_cross_entropy
 from torch.nn import BCELoss
 import time
+
+######################################
+# DATA LOADING
+######################################
 
 # Uploading ICA_files and Labels from the folder in BIDS format
 def training_data_topos(directory):
@@ -35,25 +40,23 @@ def training_data_topos(directory):
     return train_data # returns a list of lists containing Topomap feature Tensor and corrresponding labels Tensor
 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
 # Processing lists of lists into a tensor by stacking the lists vertically
 raw_data = training_data_topos('C:/Users/asaini/Desktop/FCBG/iclabel-python/BIDS_DATA/MARA_test')
 x = torch.stack([i[0] for i in raw_data])   # Tensor shape (subjects, 30, 64, 64)
 y = torch.stack([i[1] for i in raw_data])   # Tensor shape (subjects, 30)
-
-
 x_data = x.view(-1, *x.size()[2:])  # same as x_t = x.view(27*30, 64,64) # Tensor shape (subjects*30, 64, 64)
 y_data = y.view(-1)                 # # Tensor shape (subjects*30,)
-
 data = torch.utils.data.TensorDataset(x_data, y_data)
-
-BATCH_SIZE = 30    # size of a batch in training
+BATCH_SIZE = 30    # size of a batch in training = 30
 data_loader = torch.utils.data.DataLoader(data, batch_size = BATCH_SIZE)
 
-class Single_layer_net(torch.nn.Module):
+#####################################
+# MODEL
+#####################################
+class multi_layer_net(torch.nn.Module):
 
     def __init__(self, num_features):
-        super(Single_layer_net, self).__init__()
+        super(multi_layer_net, self).__init__()
 
         self.linear = torch.nn.Linear(num_features,256)
         self.linear.weight.detach().normal_(0.0, 0.1)
@@ -69,9 +72,9 @@ class Single_layer_net(torch.nn.Module):
         logits = torch.sigmoid(out)
         return logits
 
-RANDOM_SEED = 1
+RANDOM_SEED = 21
 torch.manual_seed(RANDOM_SEED)
-model = Single_layer_net(num_features=64*64).to(DEVICE)
+model = multi_layer_net(num_features=64*64).to(DEVICE)
 optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
 loss = BCELoss()
 
@@ -87,33 +90,42 @@ def compute_loss(model, data_loader):
         curr_loss += loss
     return float(curr_loss)/cnt
 
+def compute_accuracy(model, data_loader):
+    correct_pred, num_examples = 0, 0
+    for features, targets in data_loader:
+        features = features.view(-1, 64*64).to(DEVICE)
+        targets = targets.to(DEVICE)
+        logits = model(features)
+        predicted_labels = torch.where((logits >0.7), 1, 0)
+        total_IC = targets.size(0)
+        count_correct = (predicted_labels.squeeze(1) == targets).sum()
+        # predictions_data = pd.DataFrame({'Targets': targets.detach().numpy(), 'Logits': logits.detach().numpy().squeeze(1), 'Pred_labels': predicted_labels.detach().numpy().squeeze(1)})
+        print((count_correct.float()/total_IC) * 100)
+    return (count_correct.float()/total_IC) * 100
+
+####################################
+# TRAINING
+####################################
+
 NUM_EPOCHS = 100
-
-
 start_time = time.time()
 minibatch_cost = []
 epoch_cost = []
 for epoch in range(NUM_EPOCHS):
     model.train()
     for batch_idx, (features, targets) in enumerate(data_loader):
-
         features = features.view(-1, 64*64).to(DEVICE)
         targets = targets.unsqueeze(1).float().to(DEVICE)
-
         logits = model(features)
         # _, pred = torch.max(logits, 1)
         cost = loss(logits, targets)
         optimizer.zero_grad()
-
         cost.backward()
-
         optimizer.step()
-
-
         minibatch_cost.append(cost.item())
-        print ('Epoch: %03d/%03d | Batch %03d/%03d | Cost: %.4f'
-                   %(epoch+1, NUM_EPOCHS, batch_idx,
-                     len(data_loader), cost.item()))
+        print ('Epoch: %03d/%03d | Batch %03d/%03d | Cost: %.4f,'
+                   %(epoch+1, NUM_EPOCHS, batch_idx+1,
+                     len(data_loader), cost.item()), )
 
     cost = compute_loss(model, data_loader)
     epoch_cost.append(cost)
@@ -133,17 +145,5 @@ plt.plot(range(len(epoch_cost)), epoch_cost)
 plt.ylabel('Cross Entropy')
 plt.xlabel('Epoch')
 plt.show()
-
-
-def compute_accuracy(model, data_loader):
-    correct_pred, num_examples = 0, 0
-    for features, targets in data_loader:
-        features = features.view(-1, 64*64).to(DEVICE)
-        targets = targets.to(DEVICE)
-        logits = model(features)
-        predicted_labels = torch.argmax(logits, 1)
-        num_examples += targets.size(0)
-        correct_pred += (predicted_labels == targets).sum()
-    return correct_pred.float()/num_examples * 100
 
 print('Training Accuracy: %.2f' % compute_accuracy(model, data_loader))
